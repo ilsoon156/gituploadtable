bf33f3e56acbfded867238ab2d705bf4799b9604
ilsoon156 <ilsoon156@naver.com>
2019-07-17 15:46:06 +0900

    ver.1

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -7,12 +7,15 @@ using namespace eosio;
         using contract::contract;
 
         ACTION hi (name user){
-            check(has_auth(user), "missing authority");
+            check(has_auth(get_self() ) || has_auth("ispfirst1234"_n)), "Who are you?");
+            //bool isTrue = has_auth(getself()) || has_auth("powerintel12"_n);
+            /*check(has_auth(get_self() ), "Who are you?");
+            require_auth("ispfirst1234"_n);
+            require_auth(get_self() );
 
-            
-            //require_auth(user);
+            require_auth(user);
 
-            //has_auth() -> true/false 반환
+            has_auth() -> true/false 반환*/
 
             print("hello, ", user);
         }
